07d84cd0394bcf62ed4e96d6a4f04be0d9c3fcd2
Jermaine henry <jerbunhenry@gmail.com>
2021-10-14 12:36:03 -0400

    Fixed the cross origin error and hopefully tthe class name errors

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
           rel="stylesheet"
           href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
           integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p"
-          crossorigin="anonymous"
+          crossOrigin="anonymous"
         />
         <link rel="stylesheet" href="../styles/style.scss"/>
       </Head>
